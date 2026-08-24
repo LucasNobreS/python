40609204af0377ef2338afae4b443c1e60ec88d3
lucasfreesva <lucasnobre2008@outlook.com>
2026-08-24 17:47:37 -0300

    desafio poo

--- PythonPOO/Desafios.py/desafio018.py
+++ PythonPOO/Desafios.py/desafio018.py
@@ -9,7 +9,7 @@ class Churrasco:
     def analisar(self):
         preco = 400*(82.4 / 1000)
         consumo = preco * self.quant
-        analise = Panel(f"Analisando o [green]{self.titulo}[/green] com [blue]{self.quant} convidados[/blue]\n Cada participante comerá 0.4kg de carne e cada kg custa R$82,40\n Recomendo comprar [blue]{ self.quant * 0.4}kg de carne[/blue]\n O custo total sera de R$[green]{consumo: .2f}[/green]\n Cada pessoa pagara [yellow]R${consumo / self.quant: .2f}[/yellow] para participar", width=40, title=self.titulo)
+        analise = Panel(f"Analisando o [green]{self.titulo}[/green] com [blue]{self.quant} convidados[/blue]\n Cada participante comerá 0.4kg de carne e cada kg custa R$82,40\n Recomendo comprar [blue]{ self.quant * 0.4}kg de carne[/blue]\n O custo total sera de R$[green]{consumo: .2f}[/green]\n Cada pessoa pagara [yellow]R${consumo / self.quant: .2f}[/yellow] para participar", width=45, title=self.titulo)
         return analise
 
 c1 = Churrasco(15, "Churrasco")
